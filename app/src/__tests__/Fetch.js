37dce1051f4a32ef9244eb1c9956448e72f3779a
Michael <checomichael2@gmail.com>
2019-04-11 16:54:04 -0400

    complete test/mock of an axios 'get' request

--- app/src/__tests__/Fetch.js
+++ app/src/__tests__/Fetch.js
@@ -5,6 +5,16 @@ import { render, waitForElement } from 'react-testing-library';
 import axiosMock from 'axios';
 import Fetch from '../Fetch';
 const url = '/greeting';
-it('fetches data correctly', () => {
-	const { getByTestId } = render(<Fetch url={url} />);
+
+it('fetches data correctly', async () => {
+	axiosMock.get.mockResolvedValueOnce({ data: { message: 'hi' } });
+	const { getByTestId, debug } = render(<Fetch url={url} />);
+	const loadingText = getByTestId('loading');
+	debug(loadingText);
+	expect(loadingText).toHaveTextContent('Loading');
+	// debug();
+	const resolvedValue = await waitForElement(() => getByTestId('resolved'));
+	expect(resolvedValue).toHaveTextContent('hi');
+	expect(axiosMock.get).toHaveBeenCalledTimes(1);
+	expect(axiosMock.get).toHaveBeenCalledWith(url);
 });
